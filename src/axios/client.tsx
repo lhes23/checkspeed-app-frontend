096156efa8547c8557063b1c6e3e8a274fb2d3d1
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-11 06:41:33 +0800

    added ec2 public ip

--- src/axios/client.tsx
+++ src/axios/client.tsx
@@ -3,6 +3,8 @@ import axios from "axios"
 const client = axios.create()
 
 // client.defaults.baseURL = "http://localhost:8000/api/"
-client.defaults.baseURL = "http://192.168.100.30:8000/api/"
+// client.defaults.baseURL = "http://192.168.100.30:8000/api/"
+client.defaults.baseURL =
+  "http://ec2-18-142-114-177.ap-southeast-1.compute.amazonaws.com:8000/api/"
 
 export default client
