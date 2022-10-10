38880ce350e350f65b37aeb402d57b382fafaf0c
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-11 06:49:57 +0800

    add https to ec2

--- src/axios/client.tsx
+++ src/axios/client.tsx
@@ -5,6 +5,6 @@ const client = axios.create()
 // client.defaults.baseURL = "http://localhost:8000/api/"
 // client.defaults.baseURL = "http://192.168.100.30:8000/api/"
 client.defaults.baseURL =
-  "http://ec2-18-142-114-177.ap-southeast-1.compute.amazonaws.com:8000/api/"
+  "https://ec2-18-142-114-177.ap-southeast-1.compute.amazonaws.com:8000/api/"
 
 export default client
